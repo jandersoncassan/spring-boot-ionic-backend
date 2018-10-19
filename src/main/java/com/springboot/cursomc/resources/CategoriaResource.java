0568841e66800573a4481e3611f69286717e402f
Janderson Cassan <jande.max@gmail.com>
2018-10-19 16:17:19 -0300

    Testando Dominio Categoria

--- src/main/java/com/springboot/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/springboot/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,32 @@
 package com.springboot.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "funfou";
+	@GetMapping
+	public List<Categoria> listar() {
+		return getListaCategoria();
+	}
+	
+	private List<Categoria> getListaCategoria(){
+		Categoria c1 = new Categoria(1, "Informatica");
+		Categoria c2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
+		
 	}
 }
