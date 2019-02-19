2c212238ee30611ce733ee8a6a01e751b011ffd8
Janderson Cassan <jande.max@gmail.com>
2019-02-19 17:47:29 -0300

    Correcoes update categoriaService, construtor de pagamento

--- src/main/java/com/springboot/cursomc/services/CategoriaService.java
+++ src/main/java/com/springboot/cursomc/services/CategoriaService.java
@@ -41,8 +41,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return catergoriaRepository.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return catergoriaRepository.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -72,4 +73,9 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());		
+	}
+
 }
