bffb1be058adcd16e2b844b9a66f87f40f42df4f
Janderson Cassan <jande.max@gmail.com>
2019-02-19 16:28:20 -0300

    Personalizando a mensagem da validacao

--- src/main/java/com/springboot/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/springboot/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,6 +23,15 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException ex, HttpServletRequest request){
 		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> dataIntegrity(MethodArgumentNotValidException ex, HttpServletRequest request){
+		ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validacao", System.currentTimeMillis());
+		
+		ex.getBindingResult().getFieldErrors().forEach(erro -> error.addError(erro.getField(), erro.getDefaultMessage()));
+		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 
